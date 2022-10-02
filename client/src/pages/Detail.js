d3d2445b7d3c414504199387e21fdb0db2fdb1c7
Becca Doan <rebecca.doan@yahoo.com>
2022-10-02 12:19:10 -0500

    added data persistence

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -7,6 +7,8 @@ import spinner from '../assets/spinner.gif';
 import Cart from '../components/Cart'
 
 import { useStoreContext } from "../utils/GlobalState";
+import { idbPromise } from '../utils/helpers'
+
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -33,8 +35,19 @@ function Detail() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if(!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
     }
-  }, [products, data, dispatch, id]);
+    
+  }, [products, data, loading, dispatch, id]);
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id)
@@ -45,11 +58,18 @@ function Detail() {
         _id: id,
         purchaseQuantity: itemInCart.purchaseQuantity + 1
       })
+      // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: {...currentProduct, purchaseQuantity: 1}
       })
+      // if product isn't in the cart yet, add it to the current shopping cart in IndexedDB
+      idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
   }
 }
 
@@ -58,6 +78,8 @@ const removeFromCart = () => {
     type: REMOVE_FROM_CART,
     _id: currentProduct._id
   });
+  // if we're updating quantity, use existing item data and increment purchaseQuantity value by one
+  idbPromise('cart', 'delete', { currentProduct });
 };
 
 
